e2eb461db912b6887bda6e8c22928d9bea7713c6
time-go <time-go@163.com>
2014-06-18 16:35:13 +0800

    Update groot.js

--- groot.js
+++ groot.js
@@ -6,7 +6,7 @@ var groot = (function ($) {
     var RENDEAR = "Render";
     //---------------初始化对象----------------//
     function isNum(value) {
-        if (value == "")return false;
+        if (value === "")return false;
         return !isNaN(value);
     }
 
